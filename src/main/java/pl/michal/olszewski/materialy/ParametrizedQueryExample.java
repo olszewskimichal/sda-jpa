467f23ffb54bcee25b1ca6df95075ec4f6b74675
olszewskimichal <olszewskimichal@outlook.com>
2019-04-01 18:53:24 +0200

    przykladowe zapytania z parametrami

---
+++ src/main/java/pl/michal/olszewski/materialy/ParametrizedQueryExample.java
@@ -0,0 +1,27 @@
+package pl.michal.olszewski.materialy;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+public class ParametrizedQueryExample {
+
+  public static void main(String[] args) {
+    EntityManager entityManager = EntityManagerSingleton.INSTANCE.getEntityManager();
+
+    TypedQuery<Employee> query1 = entityManager
+        .createQuery("select e from Employee e where e.name = ?1", Employee.class);
+    query1.setParameter(1,"imie4");
+    List<Employee> resultList1 = query1.getResultList();
+    resultList1.stream()
+        .forEach(employee -> System.out.println(employee));
+
+    entityManager.createQuery("select e from Employee e where e.name = :employeeName", Employee.class)
+        .setParameter("employeeName","imie3")
+        .getResultList()
+        .stream()
+        .forEach(employee -> System.out.println(employee));
+
+  }
+
+}
